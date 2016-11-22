dc3e70898a78043033269c6e960f13fcba7d7957
Vildar Khisyametdinov <vildar82@gmail.com>
2016-11-22 15:46:56 +0300

    Object Converter

---
+++ NetLib/source/Converter/ConvertExt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLib
+{
+    public static class ConvertExt
+    {
+        public static T GetValue<T>(this object value)
+        {
+            var culture = CultureInfo.InvariantCulture;
+            if (value is string && typeof(T) == typeof(double))
+            {
+                value = ((string)value).Replace(",", ".");
+                culture = new CultureInfo("en-US");
+            }
+            return (T)Convert.ChangeType(value, typeof(T), culture);
+        }
+    }
+}
