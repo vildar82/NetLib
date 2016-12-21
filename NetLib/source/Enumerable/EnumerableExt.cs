8ba7144e2e6239ebcf4ab314d3d4889556be7716
Vildar Khisyametdinov <vildar82@gmail.com>
2016-12-21 15:12:09 +0300

    EnumerableExt

---
+++ NetLib/source/Enumerable/EnumerableExt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLib
+{
+    public static class EnumerableExt
+    {
+        /// <summary>
+        /// Преобразует одиночный объект в список из одного объекта (IEnumerable)
+        /// </summary>                        
+        public static IEnumerable<T> Yield<T>(this T item)
+        {
+            yield return item;
+        }
+    }
+}
