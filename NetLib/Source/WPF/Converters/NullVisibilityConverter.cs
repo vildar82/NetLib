8b308fb99ab4dba3d6fb3d9efa097472b46a61bc
Vildar <vildar82@gmail.com>
2017-05-10 11:58:01 +0300

    NullVisibilityConverter

---
+++ NetLib/Source/WPF/Converters/NullVisibilityConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NetLib.WPF.Converters
+{    
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    public class NullVisibilityConverter : ConvertorBase
+    {
+        public override object Convert (object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? Visibility.Hidden : Visibility.Visible;
+        }
+    }
+}
