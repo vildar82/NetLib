ca53e6c236b794be79f54186ba933801d26d0763
Vildar <vildar82@gmail.com>
2017-08-12 14:59:22 +0300

    Retry - Повторение операции, за заданное время и количество попыток.

---
+++ NetLib/Source/Loops/Retry.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetLib
+{
+	/// <summary>
+	/// Повторение операции, за заданное время и количество попыток.
+	/// </summary>
+	public static class Retry
+	{
+		/// <summary>
+		/// Попытки выполнить операцию
+		/// </summary>
+		/// <param name="action">Операция</param>
+		/// <param name="retryInterval">Интервал повторения</param>
+		/// <param name="retryCount">Кол попыток</param>
+		public static void Do (Action action, TimeSpan retryInterval, int retryCount = 3)
+		{
+			Do<object>(() =>
+			{
+				action();
+				return null;
+			}, retryInterval, retryCount);
+		}
+
+		/// <summary>
+		/// Попытка выполнить операцию
+		/// </summary>
+		/// <typeparam name="T">Возвращаемое значение</typeparam>
+		/// <param name="action">Операция</param>
+		/// <param name="retryInterval">Интервал повторения</param>
+		/// <param name="retryCount">Кол попыток</param>
+		/// <returns>Результат операции</returns>
+		public static T Do<T>(Func<T> action, TimeSpan retryInterval, int retryCount = 3)
+		{
+			var exceptions = new List<Exception>();
+			for (var retry = 0; retry < retryCount; retry++)
+			{
+				try
+				{
+					if (retry > 0) Thread.Sleep(retryInterval);
+					return action();
+				}
+				catch (Exception ex)
+				{
+					exceptions.Add(ex);
+				}
+			}
+			throw new AggregateException(exceptions);
+		}
+	}
+}
