5eec3dcc4b518fcfeb55f41ca166ec001fb5ff77
Vildar <vildar82@gmail.com>
2017-08-16 20:57:35 +0300

    NullCollapseVisibilityConverter

--- NetLib/Source/WPF/Converters/NullCollapseVisibilityConverter.cs
+++ NetLib/Source/WPF/Converters/NullCollapseVisibilityConverter.cs
@@ -8,14 +8,13 @@ using System.Windows;
 using System.Windows.Data;
 
 namespace NetLib.WPF.Converters
-{   
-	
+{    
     [ValueConversion(typeof(object), typeof(Visibility))]
-    public class NullVisibilityConverter : ConvertorBase
+    public class NullCollapseVisibilityConverter : ConvertorBase
     {
         public override object Convert (object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return value == null ? Visibility.Hidden : Visibility.Visible;
+	        return value == null ? Visibility.Collapsed : Visibility.Visible;
         }
     }
 }
