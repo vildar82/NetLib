6ba47f5ed83540dfea637ba87bc3937b52da31cd
Vildar <vildar82@gmail.com>
2017-08-17 10:16:25 +0300

    GridLengthConverter

---
+++ NetLib/Source/WPF/Converters/GridLengthConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace NetLib.WPF.Converters
+{    
+    [ValueConversion(typeof(double), typeof(GridLength))]
+	public class GridLengthConverter : ConvertorBase
+    {
+        public override object Convert (object value, Type targetType, object parameter, CultureInfo culture)
+        {
+			if (value is double d) return new GridLength(d);
+			throw new ArgumentException($"Ожидается double");
+        }
+
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+	        return ((GridLength)value).Value;
+        }
+    }
+}
---
+++ NetLibTests/Source/WPF/Converters/GridLengthConverterTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NetLib.WPF.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace NetLib.WPF.Converters.Tests
+{
+	[TestClass()]
+	public class GridLengthConverterTests
+	{
+		private GridLengthConverter converter;
+		[TestInitialize]
+		public void Setup()
+		{
+			converter = new GridLengthConverter();
+		}
+
+		[TestMethod()]
+		public void ConvertTest()
+		{
+			var doubleValueWidth = 100d;
+			var gridWidth =(GridLength) converter.Convert(doubleValueWidth, typeof(GridLength), null, CultureInfo.CurrentCulture);
+			Assert.AreEqual(100d, gridWidth.Value);
+		}
+
+		[TestMethod()]
+		public void ConvertBackDoubleTest()
+		{
+			var gridWidth = new GridLength(100);
+			var val = converter.ConvertBack(gridWidth, typeof(double), null, CultureInfo.CurrentCulture);
+			Assert.AreEqual(100d, val);
+		}
+	}
+}
