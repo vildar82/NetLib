dbd0b1770971430d30577cc5b8fe839437f5a1a5
Vildar <vildar82@gmail.com>
2017-10-02 11:27:15 +0300

    BindableSelectedItemBehavior

---
+++ NetLib/Source/WPF/Behaviors/TreeView/BindableSelectedItemBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace NetLib.WPF.Behaviors
+{
+    public class BindableSelectedItemBehavior : Behavior<TreeView>
+    {
+        public object SelectedItem
+        {
+            get => (object)GetValue(SelectedItemProperty);
+            set => SetValue(SelectedItemProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedItemProperty =
+            DependencyProperty.Register("SelectedItem", typeof(object), typeof(BindableSelectedItemBehavior), 
+                new UIPropertyMetadata(null, OnSelectedItemChanged));
+
+        private static void OnSelectedItemChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            var item = e.NewValue as TreeViewItem;
+            item?.SetValue(TreeViewItem.IsSelectedProperty, true);
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+
+            this.AssociatedObject.SelectedItemChanged += OnTreeViewSelectedItemChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+
+            if (this.AssociatedObject != null)
+            {
+                this.AssociatedObject.SelectedItemChanged -= OnTreeViewSelectedItemChanged;
+            }
+        }
+
+        private void OnTreeViewSelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            this.SelectedItem = e.NewValue;
+        }
+    }
+}
