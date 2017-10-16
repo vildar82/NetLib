bbbeadbc8e52c5d1ebe67d4a190d397607457b5f
Vildar <vildar82@gmail.com>
2017-10-16 13:09:22 +0300

    BoolUsage

---
+++ NetLib/Source/Bool/BoolUsage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetLib
+{
+    public class BoolUsage : IDisposable
+    {
+        private readonly bool oldValue;
+        private readonly Action<bool> setValue;
+
+        public BoolUsage(bool curValue, bool newValue, Action<bool> setValue)
+        {
+            oldValue = curValue;
+            this.setValue = setValue;
+            setValue(newValue);
+        }
+
+        public void Dispose()
+        {
+            setValue(oldValue);
+        }
+    }
+}
