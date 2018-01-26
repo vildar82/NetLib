d13ef34f2144dde3f54d46a6148e5e5c9e923808
Vildar <vildar82@gmail.com>
2018-01-26 17:14:37 +0300

    remove CustomRichTextBox

--- NetLib/Properties/AssemblyInfo.cs
+++ NetLib/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.26.84")]
-[assembly: AssemblyFileVersion("1.0.26.84")]
+[assembly: AssemblyVersion("1.0.26.85")]
+[assembly: AssemblyFileVersion("1.0.26.85")]
--- WpfApp1/MainViewModel.cs
+++ WpfApp1/MainViewModel.cs
@@ -8,57 +8,11 @@ namespace WpfApp1
 {
     public class MainViewModel : BaseViewModel
     {
-        public ReactiveCommand ShowDialog { get; set; }
-
-        public ReactiveCommand ShowWindow1 { get; set; }
-        public ReactiveCommand TestProgress { get; set; }
-
-        public UserControlViewModel UserControl { get; set; }
+        public string Message { get; set; }
 
         public MainViewModel()
         {
-            UserControl = new UserControlViewModel(this);
-            ShowDialog = CreateCommand(ShowDialogExec);
-            TestProgress = CreateCommand(TestProgressExec);
-            ShowWindow1 = CreateCommand(ShowWindow1Exec);
-        }
-
-        private void ShowDialogExec()
-        {
-            var dialogView = new DialogView(new DialogViewModel());
-            dialogView.ShowDialog();
-        }
-
-        private void ShowWindow1Exec()
-        {
-            var win = new Window1();
-            win.Show();
-        }
-
-        private async void TestProgressExec()
-        {
-            // ReSharper disable once NotAccessedVariable
-            var res = await ShowMessage("Test", "Msg", "Yes", "No");
-            // ReSharper disable once RedundantAssignment
-            res = await ShowMessage("Test", "Msg", "Yes", "No", "Cancel");
-            Debug.WriteLine($"{Thread.CurrentThread.ManagedThreadId} TestProgressExec start");
-            await ShowProgressDialog(c =>
-            {
-                c.SetIndeterminate();
-                Debug.WriteLine($"{Thread.CurrentThread.ManagedThreadId} TestProgressExec progress start");
-                for (var i = 0; i < 200000000; i++)
-                {
-                    if (i % 10000000 == 0) DoEvents();
-                    // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
-                    Math.Sqrt(i);
-                    // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
-                    Math.Sqrt(i / (double)i);
-                    // ReSharper disable once ReturnValueOfPureMethodIsNotUsed
-                    Math.Sqrt(i * i);
-                }
-                Debug.WriteLine($"{Thread.CurrentThread.ManagedThreadId} TestProgressExec progress end");
-            }, "Заголовок", "Сообщение", false);
-            Debug.WriteLine($"{Thread.CurrentThread.ManagedThreadId} TestProgressExec end");
+            Message = "Продуктом обновлена поставка КПП/СПП/БПП от 24.01.2018.\r\nНаша библиотека и ТЭП соответствует поставке от 26.12.2017.\r\nВедутся работы по обновлению в плановом порядке.";
         }
     }
 }
