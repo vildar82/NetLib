81d42a80d9e568ee51db1dd99452570aa7723b05
Vildar <vildar82@gmail.com>
2018-03-03 17:12:20 +0300

    monad try

--- NetLib/Properties/AssemblyInfo.cs
+++ NetLib/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.59.90")]
-[assembly: AssemblyFileVersion("1.0.59.90")]
+[assembly: AssemblyVersion("1.0.62.92")]
+[assembly: AssemblyFileVersion("1.0.62.92")]
---
+++ NetLib/Source/Monad/MonadExt.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using NLog;
+
+namespace NetLib.Monad
+{
+    public static class MonadExt
+    {
+        private static ILogger Logger { get; } = LogManager.GetCurrentClassLogger();
+
+        public static T Try<T>(this T obj, Action<T> action, [CanBeNull] Action<T, Exception> exception = null)
+        {
+            try
+            {
+                action(obj);
+            }
+            catch (Exception ex)
+            {
+                if (exception == null) Logger.Error(ex);
+                else exception(obj, ex);
+            }
+            return obj;
+        }
+    }
+}
