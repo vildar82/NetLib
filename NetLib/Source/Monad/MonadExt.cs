0066e8347b73bdf4955c04b0169a654a12e520a3
Vildar <vildar82@gmail.com>
2018-03-22 17:21:17 +0300

    release

--- NetLib/Source/Monad/MonadExt.cs
+++ NetLib/Source/Monad/MonadExt.cs
@@ -10,6 +10,14 @@ namespace NetLib.Monad
     {
         private static ILogger Logger { get; } = LogManager.GetCurrentClassLogger();
 
+        /// <summary>
+        /// Попытка работы с объектом, возвращается сам объект
+        /// </summary>
+        /// <typeparam name="T">Тип объекта</typeparam>
+        /// <param name="obj">Объект</param>
+        /// <param name="action">Работа</param>
+        /// <param name="exception">обработка исключения при работе</param>
+        /// <returns>Объект</returns>
         public static T Try<T>(this T obj, Action<T> action, [CanBeNull] Action<T, Exception> exception = null)
         {
             try
@@ -24,7 +32,16 @@ namespace NetLib.Monad
             return obj;
         }
 
-        public static TRes Try<T,TRes>(this T obj, Func<T, TRes> func, Func<Exception, TRes> exception)
+        /// <summary>
+        /// Попытка работы с объектом, возвращается результат
+        /// </summary>
+        /// <typeparam name="T">Тип объекта</typeparam>
+        /// <typeparam name="TRes">Тип результата</typeparam>
+        /// <param name="obj">Объект</param>
+        /// <param name="func">Работа</param>
+        /// <param name="exception">Обработка исключения</param>
+        /// <returns></returns>
+        public static TRes Try<T,TRes>(this T obj, Func<T, TRes> func, [CanBeNull] Func<Exception, TRes> exception=null)
         {
             try
             {
@@ -32,7 +49,7 @@ namespace NetLib.Monad
             }
             catch (Exception ex)
             {
-                return exception(ex);
+                return exception == null ? default : exception(ex);
             }
         }
     }
