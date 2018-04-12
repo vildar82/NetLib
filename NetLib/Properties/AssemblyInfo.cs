699cb4cbfc2f5e0d09e404a30fec708ae15fd012
Vildar <vildar82@gmail.com>
2018-04-12 15:31:57 +0300

    IsInInterval

--- NetLib/Properties/AssemblyInfo.cs
+++ NetLib/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.95.124")]
-[assembly: AssemblyFileVersion("1.0.95.124")]
-[assembly: AssemblyInformationalVersion("1.0.2")]
+[assembly: AssemblyVersion("1.0.102.126")]
+[assembly: AssemblyFileVersion("1.0.102.126")]
+[assembly: AssemblyInformationalVersion("1.0.4")]
--- NetLib/Source/Math/DoubleExt.cs
+++ NetLib/Source/Math/DoubleExt.cs
@@ -7,6 +7,18 @@ namespace NetLib
     public static class DoubleExt
     {
         /// <summary>
+        /// Значение входит в интервал?
+        /// </summary>
+        /// <param name="val">Значение</param>
+        /// <param name="start">Стартовое значение интервала</param>
+        /// <param name="end">Конечное значение интервала</param>
+        /// <returns></returns>
+        public static bool IsInInterval(this double val, double start, double end)
+        {
+            return val >= start && val <= end;
+        }
+
+        /// <summary>
         /// Сравнение двух чисел, допустимое расхождение 0.0000001 (6 нулей)
         /// </summary>
         /// <param name="value1"></param>
