639440e0ea8985005f15d1bdd88ee234871d5d27
Vildar <vildar82@gmail.com>
2018-05-08 20:24:19 +0300

    fix BaseModel - работа без базовой модели.

--- NetLib/Source/WPF/BaseModel.cs
+++ NetLib/Source/WPF/BaseModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using JetBrains.Annotations;
+using NLog;
 using ReactiveUI;
 
 namespace NetLib.WPF
@@ -11,6 +13,8 @@ namespace NetLib.WPF
     [PublicAPI]
     public abstract class BaseModel : ReactiveObject, IBaseModel
     {
+        protected static Logger Logger { get; } = LogManager.GetCurrentClassLogger();
+
         protected BaseModel()
         {
             
@@ -26,18 +30,32 @@ namespace NetLib.WPF
         [NotNull]
         public ReactiveCommand CreateCommand(Action execute, IObservable<bool> canExecute = null)
         {
-            return BaseVm.CreateCommand(execute, canExecute);
+             if (BaseVm != null) return BaseVm.CreateCommand(execute, canExecute);
+            var command = ReactiveCommand.Create(execute, canExecute);
+            command.ThrownExceptions.Subscribe(CommandException);
+            return command;
         }
         
         [NotNull]
         public ReactiveCommand CreateCommand<T>(Action<T> execute, IObservable<bool> canExecute = null)
         {
-            return BaseVm.CreateCommand(execute, canExecute);
+            if (BaseVm != null) return BaseVm.CreateCommand(execute, canExecute);
+            var command = ReactiveCommand.Create(execute, canExecute);
+            command.ThrownExceptions.Subscribe(CommandException);
+            return command;
+        }
+
+        public void CommandException([NotNull] Exception e)
+        {
+            if (e is OperationCanceledException) return;
+            Logger.Error(e, "CommandException");
+            ShowMessage(e.Message);
         }
 
         public void ShowMessage(string msg, string title = "Ошибка")
         {
-            BaseVm.ShowMessage(msg, title);
+            if (BaseVm != null) BaseVm.ShowMessage(msg, title);
+            else MessageBox.Show(msg, title);
         }
 
         public virtual void OnPropertyChanged([CanBeNull] [CallerMemberName] string propertyName = null)
