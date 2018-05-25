5f75ed9c8cb2f58b59455b9111facadd1547f1d5
Vildar <vildar82@gmail.com>
2018-05-25 16:51:20 +0300

    Обновил до NET4.6.2 и ReactiveUI8

--- NetLib/Properties/AssemblyInfo.cs
+++ NetLib/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.145.305")]
-[assembly: AssemblyFileVersion("1.0.145.305")]
-[assembly: AssemblyInformationalVersion("1.0.183")]
+[assembly: AssemblyVersion("1.0.145.306")]
+[assembly: AssemblyFileVersion("1.0.145.306")]
+[assembly: AssemblyInformationalVersion("1.0.184")]
--- ProgressUtils/Properties/AssemblyInfo.cs
+++ ProgressUtils/Properties/AssemblyInfo.cs
@@ -49,7 +49,7 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.145.45")]
-[assembly: AssemblyFileVersion("1.0.145.45")]
+[assembly: AssemblyVersion("1.0.145.46")]
+[assembly: AssemblyFileVersion("1.0.145.46")]
 
-[assembly: AssemblyInformationalVersion("1.0.45")]
+[assembly: AssemblyInformationalVersion("1.0.46")]
