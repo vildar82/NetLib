db9f5e0cceb0e14773e22c1ba14e0314f2e4ce63
Vildar <vildar82@gmail.com>
2018-07-07 18:10:51 +0300

    NotNullToRedConverter

---
+++ NetLib/Source/WPF/Converters/NotNullToRedConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using JetBrains.Annotations;
+
+namespace NetLib.WPF.Converters
+{
+    [PublicAPI]
+    [ValueConversion(typeof(object), typeof(Brush))]
+    public class NotNullToRedConverter : ConvertorBase
+    {
+        private static readonly Brush red = new SolidColorBrush(Colors.Red);
+
+        public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value == null ? null : red;
+        }
+    }
+}
