bd16591616782766e2d3e37f3d20430766617955
Vildar <vildar82@gmail.com>
2018-07-27 10:04:00 +0300

    rebuild

--- NetLib/Properties/AssemblyInfo.cs
+++ NetLib/Properties/AssemblyInfo.cs
@@ -1,36 +1,14 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
-
-// Управление общими сведениями о сборке осуществляется с помощью
-// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
-// связанные со сборкой.
 [assembly: AssemblyTitle("NetLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("C# Lib for NET4.6.2 WPF, ReactiveUI...")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NetLib")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми
-// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через
-// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
 [assembly: ComVisible(false)]
-
-// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
 [assembly: Guid("0ba665f1-3e18-4fd8-88cd-9df53fe60647")]
-
-// Сведения о версии сборки состоят из следующих четырех значений:
-//
-//      Основной номер версии
-//      Дополнительный номер версии
-//   Номер сборки
-//      Редакция
-//
-// Можно задать все значения или принять номера сборки и редакции по умолчанию
-// используя "*", как показано ниже:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.208.488")]
-[assembly: AssemblyFileVersion("1.0.208.488")]
-[assembly: AssemblyInformationalVersion("1.0.184")]
+[assembly: AssemblyVersion("1.0.208.497")]
+[assembly: AssemblyFileVersion("1.0.208.497")]
