23fe77c2933dd7e894d31a18975ab875011442c8
Vildar <vildar82@gmail.com>
2018-07-30 09:55:48 +0300

    ImageExt ImageToByteArray

---
+++ NetLib/Source/Images/ImageExt.cs
@@ -0,0 +1,26 @@
+﻿using System.Drawing;
+using System.IO;
+
+namespace NetLib.Images
+{
+    public static class ImageExt
+    {
+        public static byte[] ImageToByteArray(this Image image)
+        {
+            using (var ms = new MemoryStream())
+            {
+                image.Save(ms, System.Drawing.Imaging.ImageFormat.Gif);
+                return ms.ToArray();
+            }
+        }
+
+        public static Image ByteArrayToImage(this byte[] byteArray)
+        {
+            using (var ms = new MemoryStream(byteArray))
+            {
+                var image = Image.FromStream(ms);
+                return image;
+            }
+        }
+    }
+}
