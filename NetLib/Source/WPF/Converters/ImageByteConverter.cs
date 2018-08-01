ab9877b205d1841c31faa612aa9a6049ebdb2485
Vildar <vildar82@gmail.com>
2018-08-01 09:48:02 +0300

    ImageByteConverter

---
+++ NetLib/Source/WPF/Converters/ImageByteConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+using NetLib.WPF.Data;
+
+namespace NetLib.WPF.Converters
+{
+    [ValueConversion(typeof(byte[]), typeof(ImageSource))]
+    public class ImageByteConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is byte[] array)
+            {
+                return array.ToImage();
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
