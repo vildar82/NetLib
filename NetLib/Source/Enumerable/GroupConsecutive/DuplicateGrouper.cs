fdfd7091f3e44c02896fd386c777c589ac6c4e99
Vildar <vildar82@gmail.com>
2018-08-13 20:32:16 +0300

    GroupConsecutive

---
+++ NetLib/Source/Enumerable/GroupConsecutive/DuplicateGrouper.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace NetLib
+{
+    internal class DuplicateGrouper<T>
+    {
+        private T CurrentKey;
+        private IEnumerator<T> Itr;
+        private bool More;
+
+        public IEnumerable<IEnumerable<T>> GroupByDuplicate(IEnumerable<T> src)
+        {
+            using(Itr = src.GetEnumerator())
+            {
+                More = Itr.MoveNext();
+                while (More)
+                    yield return GetDuplicates();
+            }
+        }
+
+        private IEnumerable<T> GetDuplicates()
+        {
+            CurrentKey = Itr.Current;
+            while (More && CurrentKey.Equals(Itr.Current))
+            {
+                yield return Itr.Current;
+                More = Itr.MoveNext();
+            }
+        }
+    }
+}
---
+++ NetLib/Source/Enumerable/GroupConsecutive/PartitionExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace NetLib
+{
+    public static class PartitionExtension
+    {
+        /// <summary>
+        /// Группировка последовательно совпадающих значений
+        /// </summary>
+        /// <typeparam name="T">Тип значения</typeparam>
+        /// <param name="src">Последовательность значений</param>
+        /// <returns>Группировка</returns>
+        public static IEnumerable<IEnumerable<T>> GroupConsecutive<T>(this IEnumerable<T> src)
+        {
+            var grouper = new DuplicateGrouper<T>();
+            return grouper.GroupByDuplicate(src);
+        }
+    }
+}
