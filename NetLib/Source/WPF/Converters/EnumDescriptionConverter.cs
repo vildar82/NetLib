60c4c505589e6c72c10c6931b05d924128aa3a46
Vildar <vildar82@gmail.com>
2018-08-31 20:28:35 +0300

    EnumDescriptionConverter

--- NetLib/Source/WPF/Converters/EnumDescriptionConverter.cs
+++ NetLib/Source/WPF/Converters/EnumDescriptionConverter.cs
@@ -1,6 +1,7 @@
 ﻿using JetBrains.Annotations;
 using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
 
 namespace NetLib.WPF.Converters
@@ -12,6 +13,24 @@ namespace NetLib.WPF.Converters
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return value.Description();
+        }
+
+        /// <inheritdoc />
+        public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string s && targetType.IsEnum)
+            {
+                foreach (var enumValue in targetType.GetEnumValues())
+                {
+                    var desc = value.Description();
+                    if (desc == s)
+                    {
+                        return enumValue;
+                    }
+                }
+            }
+
+            return DependencyProperty.UnsetValue;
         }
     }
 }
