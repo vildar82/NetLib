6ea46d7ef47e1766a30b38b7591bd1459889488b
Vildar <vildar82@gmail.com>
2018-09-13 10:08:02 +0300

    XmlExt

---
+++ NetLib/Source/Files/XmlExt.cs
@@ -0,0 +1,29 @@
+﻿using JetBrains.Annotations;
+
+namespace NetLib
+{
+    [PublicAPI]
+    public static class XmlExt
+    {
+        /// <summary>
+        /// Серилизовать объект в Xml
+        /// </summary>
+        /// <typeparam name="T">Тип объекта</typeparam>
+        /// <param name="obj">Объект</param>
+        /// <param name="file">Файл</param>
+        public static void ToXml<T>([NotNull] this T obj, string file)
+        {
+            SerializerXml.Save(file, obj);
+        }
+
+        /// <summary>
+        /// Десирилизовать объект из файла xml
+        /// </summary>
+        /// <typeparam name="T">Тип объекта</typeparam>
+        /// <param name="file">Файл xml</param>
+        public static T FromXml<T>([NotNull] this string file) where T : class, new()
+        {
+            return SerializerXml.Load<T>(file);
+        }
+    }
+}
