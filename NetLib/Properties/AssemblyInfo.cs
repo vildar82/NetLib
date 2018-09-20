3e93895e74d958e74c68fb47756912a5aac8f7eb
Vildar <vildar82@gmail.com>
2018-09-20 19:28:06 +0300

    update

--- NetLib/Properties/AssemblyInfo.cs
+++ NetLib/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0ba665f1-3e18-4fd8-88cd-9df53fe60647")]
-[assembly: AssemblyVersion("1.0.256.543")]
-[assembly: AssemblyFileVersion("1.0.256.543")]
+[assembly: AssemblyVersion("1.0.263.544")]
+[assembly: AssemblyFileVersion("1.0.263.544")]
