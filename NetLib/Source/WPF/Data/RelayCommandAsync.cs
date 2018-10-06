fa5671556742fb66aba42ddb74d821532ab0ab47
Vildar <vildar82@gmail.com>
2018-10-06 13:53:44 +0300

    RelayCommandAsync

---
+++ NetLib/Source/WPF/Data/RelayCommandAsync.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace NetLib.WPF.Data
+{
+    
+    public class RelayCommandAsync<T> : RelayCommand<T>
+	{
+		private bool isExecuting;
+		public event EventHandler Started;
+		public event EventHandler Ended;
+
+		public bool IsExecuting => isExecuting;
+
+		public RelayCommandAsync(Action<T> execute, Predicate<T> canExecute = null)
+			: base(execute, canExecute)
+		{
+		}		
+
+		public override bool CanExecute(object parameter)
+		{
+			return base.CanExecute(parameter) && !isExecuting;
+		}
+
+		public override void Execute(object parameter)
+		{
+			try
+			{
+				isExecuting = true;
+				Started?.Invoke(this, EventArgs.Empty);
+
+				var task = Task.Factory.StartNew(() =>
+				{
+					base.Execute(parameter);
+				});
+				task.ContinueWith(t =>
+				{
+					OnRunWorkerCompleted(EventArgs.Empty);
+				}, TaskScheduler.FromCurrentSynchronizationContext());
+			}
+			catch (Exception ex)
+			{
+				OnRunWorkerCompleted(new RunWorkerCompletedEventArgs(null, ex, true));
+			}
+		}
+
+		private void OnRunWorkerCompleted(EventArgs e)
+		{
+			isExecuting = false;
+			Ended?.Invoke(this, e);
+		}
+	}
+
+	public class RelayCommandAsync : RelayCommand
+    {
+        private bool isExecuting;
+        public event EventHandler Started;
+        public event EventHandler Ended;
+
+        public bool IsExecuting => isExecuting;
+
+        public RelayCommandAsync(Action execute, Func<bool> canExecute)
+            : base(execute, canExecute)
+        {
+        }
+
+        public RelayCommandAsync(Action execute)
+            : base(execute)
+        {
+        }
+
+        public override bool CanExecute(object parameter)
+        {
+            return base.CanExecute(parameter) && !isExecuting;
+        }
+
+        public override void Execute(object parameter)
+        {
+            try
+            {
+                isExecuting = true;
+                Started?.Invoke(this, EventArgs.Empty);
+
+                var task = Task.Factory.StartNew(() =>
+                {
+                    _execute();
+                });
+                task.ContinueWith(t =>
+                {
+                    OnRunWorkerCompleted(EventArgs.Empty);
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+            }
+            catch (Exception ex)
+            {
+                OnRunWorkerCompleted(new RunWorkerCompletedEventArgs(null, ex, true));
+            }
+        }
+
+        private void OnRunWorkerCompleted(EventArgs e)
+        {
+            isExecuting = false;
+            Ended?.Invoke(this, e);
+        }
+    }
+}
