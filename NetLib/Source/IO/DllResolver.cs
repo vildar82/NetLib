53f5f93e0a34c147af6aaef9cdd8551ef3ade861
Vildar <vildar82@gmail.com>
2018-10-25 13:58:18 +0300

    DllResolve - filters

--- NetLib/Source/IO/DllResolver.cs
+++ NetLib/Source/IO/DllResolver.cs
@@ -6,6 +6,8 @@ using System.Reflection;
 
 namespace NetLib.IO
 {
+    using System.Linq;
+
     /// <summary>
     /// Разрешение сборки
     /// </summary>
@@ -14,6 +16,7 @@ namespace NetLib.IO
     public class DllResolve
     {
         public string DllName { get; set; }
+        
         public string DllFile { get; set; }
 
         public DllResolve(string dllFile)
@@ -25,13 +28,25 @@ namespace NetLib.IO
         [NotNull]
         public static List<DllResolve> GetDllResolve([NotNull] string dllFolder, SearchOption mode)
         {
+            return GetDllResolve(dllFolder, mode, "dll");
+        }
+
+        [NotNull]
+        public static List<DllResolve> GetDllResolve([NotNull] string dllFolder, SearchOption mode, params string[] extFilter)
+        {
             var dllResolves = new List<DllResolve>();
-            foreach (var dllFile in Directory.EnumerateFiles(dllFolder, "*.dll", mode))
+            foreach (var dllFile in Directory.EnumerateFiles(dllFolder, "*", mode).Where(f => IsFilterExt(f, extFilter)))
             {
                 var dllResolve = new DllResolve(dllFile);
                 dllResolves.Add(dllResolve);
             }
             return dllResolves;
+        }
+
+        private static bool IsFilterExt(string file, string[] extFilter)
+        {
+            var ext = System.IO.Path.GetExtension(file);
+            return extFilter.Any(a => a.EqualsIgnoreCase(ext));
         }
 
         public bool IsResolve([NotNull] string dllRequest)
