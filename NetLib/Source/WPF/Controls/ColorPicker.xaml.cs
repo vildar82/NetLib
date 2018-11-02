b7ff247282d7187e252c9e2d8d39b7c60fbd08c4
Vildar <vildar82@gmail.com>
2018-11-02 11:32:32 +0300

    add ColorPicker

---
+++ NetLib/Source/WPF/Controls/ColorPicker.xaml.cs
@@ -0,0 +1,72 @@
+namespace NetLib.WPF.Controls
+{
+    using System.ComponentModel;
+    using System.Drawing;
+    using System.Runtime.CompilerServices;
+    using System.Windows;
+    using System.Windows.Forms;
+    using JetBrains.Annotations;
+    using UserControl = System.Windows.Controls.UserControl;
+
+    public partial class ColorPicker : UserControl, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Цвет (AutoCAD)
+        /// </summary>
+        public static readonly DependencyProperty ColorProperty =
+            DependencyProperty.Register("Color", typeof(Color), typeof(ColorPicker),
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        private bool canClearColor;
+
+        public ColorPicker()
+        {
+            InitializeComponent();
+            CanClearColor = true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool CanClearColor
+        {
+            get => canClearColor;
+            set
+            {
+                if (value == canClearColor)
+                    return;
+                canClearColor = value;
+                OnPropertyChanged();
+            }
+        }
+        
+        public Color Color
+        {
+            get => (Color)GetValue(ColorProperty);
+            set
+            {
+                SetValue(ColorProperty, value);
+                CanClearColor = Color != null;
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CanBeNull] [CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            Color = Color.Empty;
+        }
+
+        private void SelectColor(object sender, RoutedEventArgs e)
+        {
+            var colorDlg = new ColorDialog { Color = Color };
+            if (colorDlg.ShowDialog() == DialogResult.OK)
+            {
+                Color = colorDlg.Color;
+            }
+        }
+    }
+}
