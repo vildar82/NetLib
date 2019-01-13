45c874916463712740a2d9dbc444a7e028bebff1
Vildar <vildar82@gmail.com>
2019-01-13 17:07:21 +0300

    imagesource ResizedImage

--- NetLib/Source/Images/ImageExt.cs
+++ NetLib/Source/Images/ImageExt.cs
@@ -4,6 +4,9 @@
     using System.Drawing.Drawing2D;
     using System.Drawing.Imaging;
     using System.IO;
+    using System.Windows;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
     using JetBrains.Annotations;
 
     [PublicAPI]
@@ -54,6 +57,23 @@
             }
 
             return destImage;
+        }
+
+        public static BitmapFrame ResizedImage(ImageSource source, int width, int height, int margin)
+        {
+            var rect = new Rect(margin, margin, width - margin * 2, height - margin * 2);
+            var group = new DrawingGroup();
+            RenderOptions.SetBitmapScalingMode(group, BitmapScalingMode.HighQuality);
+            group.Children.Add(new ImageDrawing(source, rect));
+            var drawingVisual = new DrawingVisual();
+            using (var drawingContext = drawingVisual.RenderOpen())
+                drawingContext.DrawDrawing(group);
+            var resizedImage = new RenderTargetBitmap(
+                width, height,         // Resized dimensions
+                96, 96,                // Default DPI values
+                PixelFormats.Default); // Default pixel format
+            resizedImage.Render(drawingVisual);
+            return BitmapFrame.Create(resizedImage);
         }
     }
 }
