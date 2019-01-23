faacada7969ea9f3e07230ca7041305d6a2d80fd
Vildar <vildar82@gmail.com>
2019-01-23 10:03:58 +0300

    fix BaseModel CreateCommandAsync TResult

--- NetLib/Source/WPF/BaseModel.cs
+++ NetLib/Source/WPF/BaseModel.cs
@@ -108,6 +108,14 @@
             return command;
         }
 
+        [NotNull]
+        public ReactiveCommand<Unit, TResult> CreateCommandAsync<TResult>(Func<Task<TResult>> execute, IObservable<bool> canExecute = null)
+        {
+            var command = ReactiveCommand.CreateFromTask(execute, canExecute, new DispatcherScheduler(Dispatcher.CurrentDispatcher));
+            command.ThrownExceptions.Subscribe(CommandException);
+            return command;
+        }
+
         public void CommandException([NotNull] Exception e)
         {
             if (e is OperationCanceledException) return;
