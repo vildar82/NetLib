62da8ebe3e722c969892f4c9c9a2e61f0d50e945
Vildar <vildar82@gmail.com>
2019-01-31 09:46:08 +0300

    MouseOverToScrollbarVisibility

---
+++ NetLib/Source/WPF/Converters/MouseOverToScrollbarVisibility.cs
@@ -0,0 +1,25 @@
+namespace NetLib.WPF.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// При навыедении мышки показывать скролбар
+    /// ScrollViewer.VerticalScrollBarVisibility="{Binding IsMouseOver, Converter={StaticResource MouseOverToScrollBarVisibility}}">
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(ScrollBarVisibility))]
+    sealed class MouseOverToScrollbarVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return ((bool)value) ? ScrollBarVisibility.Auto : ScrollBarVisibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
