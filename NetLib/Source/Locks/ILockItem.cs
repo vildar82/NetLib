6af84a93f2ca2aa44d97a5544b6c911254ef8751
Vildar <vildar82@gmail.com>
2019-03-08 13:21:20 +0300

    471 u

--- NetLib/Source/Locks/ILockItem.cs
+++ NetLib/Source/Locks/ILockItem.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using JetBrains.Annotations;
-
-namespace NetLib.Locks
+﻿namespace NetLib.Locks
 {
+    using System;
+    using JetBrains.Annotations;
+
     [PublicAPI]
     public interface ILockItem : IDisposable
     {
         LockInfo Info { get; }
         bool IsLockSuccess { get; }
-
         string GetMessage();
     }
 }
