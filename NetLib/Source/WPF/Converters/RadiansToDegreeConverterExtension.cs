4449f781fd101e14995ebf4452b8bcf0b39836e0
Vildar <vildar82@gmail.com>
2019-04-04 11:20:47 +0300

    fix RadiansToDegreeConverterExtension null

--- NetLib/Source/WPF/Converters/RadiansToDegreeConverterExtension.cs
+++ NetLib/Source/WPF/Converters/RadiansToDegreeConverterExtension.cs
@@ -8,11 +8,13 @@ namespace NetLib.WPF.Converters
     [PublicAPI]
     [ValueConversion(typeof(int), typeof(string))]
     [ValueConversion(typeof(double), typeof(string))]
+    [ValueConversion(typeof(double?), typeof(string))]
     public class RadiansToDegreeConverterExtension : ConvertorBase
     {
         [NotNull]
         public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (value == null) return null;
             double date = System.Convert.ToDouble(value);
             return date.ToDegrees().ToString("N2");
         }
@@ -20,6 +22,7 @@ namespace NetLib.WPF.Converters
         [NotNull]
         public override object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            if (value == null) return null;
             double date = System.Convert.ToDouble(value);
             return date.ToRadians();
         }
