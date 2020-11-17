be256b59a73779a6c740d4158b6561c08c5e2de0
Vildar <vildar82@gmail.com>
2020-11-17 10:06:15 +0300

    exception ext

--- NetLib/Properties/AssemblyInfo.cs
+++ NetLib/Properties/AssemblyInfo.cs
@@ -1,14 +1,14 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NetLib")]
-[assembly: AssemblyDescription("C# Lib for NET4.6.2 WPF, ReactiveUI...")]
+[assembly: AssemblyDescription("C# Lib for NET4.7.2 WPF, ReactiveUI...")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NetLib")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("0ba665f1-3e18-4fd8-88cd-9df53fe60647")]
-[assembly: AssemblyVersion("1.2.3.0")]
-[assembly: AssemblyFileVersion("1.2.3.0")]
+[assembly: AssemblyVersion("1.2.4.0")]
+[assembly: AssemblyFileVersion("1.2.4.0")]
---
+++ NetLib/Source/Errors/ExtensionExt.cs
@@ -0,0 +1,32 @@
+﻿namespace NetLib.Errors
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// Расширения для исключений
+    /// </summary>
+    public static class ExceptionExt
+    {
+        /// <summary>
+        /// Сообщение об ошибке включая вложенные исключения и аггркгатные.
+        /// </summary>
+        public static string GetMessageWithInner(this Exception ex)
+        {
+            var msg = ex.Message;
+            var inner = ex.InnerException;
+            while (inner != null)
+            {
+                msg += $". {inner.Message}";
+                inner = inner.InnerException;
+            }
+
+            if (ex is AggregateException ag)
+            {
+                msg = ag.InnerExceptions.Aggregate(msg, (current, agInner) => current + $". {GetMessageWithInner(agInner)}");
+            }
+
+            return msg;
+        }
+    }
+}
