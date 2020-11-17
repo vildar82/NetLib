48fc4bf68d1689f5024d9454319b1e8129eb5f45
Vildar <vildar82@gmail.com>
2020-11-17 11:26:53 +0300

    exception inner messaage

--- NetLib/Source/Errors/ExtensionExt.cs
+++ NetLib/Source/Errors/ExtensionExt.cs
@@ -17,16 +17,22 @@
             var inner = ex.InnerException;
             while (inner != null)
             {
-                msg += $". {inner.Message}";
+                msg = AppendMessage(msg, inner.Message);
                 inner = inner.InnerException;
             }
 
             if (ex is AggregateException ag)
             {
-                msg = ag.InnerExceptions.Aggregate(msg, (current, agInner) => current + $". {GetMessageWithInner(agInner)}");
+                msg = ag.InnerExceptions.Aggregate(msg,
+                    (current, agInner) => AppendMessage(current, GetMessageWithInner(agInner)));
             }
 
             return msg;
+        }
+
+        private static string AppendMessage(string message, string append)
+        {
+            return message.Contains(append) ? message : $"{message}{(message.EndsWith(".") ? "" : ".")} {append}";
         }
     }
 }
